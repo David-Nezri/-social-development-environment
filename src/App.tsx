2a15962435163c1c8c376f3f4cb73ca867bcd87a
David <davidnezri1990@gmail.com>
2023-10-29 18:08:34 +0200

    initial esbuild service

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import * as esbuild from 'esbuild-wasm'
 import './App.css';
+import { useState  , useEffect , useRef} from 'react';
 
 function App() {
+const ref = useRef<any>();
+const [input , setInput] = useState('');
+const [code , setCode] = useState('');
+
+const startService = async () => {
+      ref.current = await esbuild.startService({
+        worker:true , 
+        wasmURL: '/esbuild.wasm'
+      });
+     
+};
+
+useEffect(() =>{
+ startService();
+},[])
+
+const onClick = () => {
+    if(!ref.current){
+       return;
+    }
+   console.log(ref.current);
+};
+
   return (
     <div className="App">
-       <h1>hi there</h1>
+       <div>
+        <textarea onChange={(event) => setInput(event.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+       </div>
     </div>
   );
 }

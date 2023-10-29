9d0ecc7e69e98f3c420438b6deb7f23148caa734
David <davidnezri1990@gmail.com>
2023-10-29 19:09:15 +0200

    implement es2015 & present in browser

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,15 @@ useEffect(() =>{
  startService();
 },[])
 
-const onClick = () => {
+const onClick = async () => {
     if(!ref.current){
        return;
     }
-   console.log(ref.current);
+   const result = await ref.current.transform(input , {
+    loader: 'jsx',
+    target: 'es2015'
+   });
+   setCode(result.code);
 };
 
   return (
